ce4c8ade3e6d1e1f6c907f5be8588bd825868e1b
Nayane Santos <nayanemenezes2011@gmail.com>
2022-02-10 21:52:40 -0300

    responsive

--- js/script.js
+++ js/script.js
@@ -9,14 +9,44 @@ fetch(url).then(response => {
 
 
 function moviesLoad(data){
-    let teste = document.getElementById('teste')
     const img = "https://image.tmdb.org/t/p/w200";
-    console.log(data.results.map(s => s))
+    console.log($('#content'))
 
     data.results.map(s => {
-            let div = document.createElement('div')
-            div.innerHTML = `<img src='${img}${s.poster_path}' />`
-            teste.appendChild(div)
+            $(".box-1").append(`<img src="${img}${s.poster_path}" class="item"/>`)
     })
 }
 
+
+const gap = 16;
+
+const carousel = document.getElementById("carousel"),
+  content = document.getElementById("content"),
+  next = document.getElementById("next"),
+  prev = document.getElementById("prev");
+
+next.addEventListener("click", e => {
+  carousel.scrollBy(width + gap, 0);
+  if (carousel.scrollWidth !== 0) {
+    prev.style.display = "flex";
+  }
+  if (content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+    next.style.display = "none";
+  }
+});
+prev.addEventListener("click", e => {
+  carousel.scrollBy(-(width + gap), 0);
+  if (carousel.scrollLeft - width - gap <= 0) {
+    prev.style.display = "none";
+  }
+  if (!content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+    next.style.display = "flex";
+  }
+});
+
+let width = carousel.offsetWidth;
+window.addEventListener("resize", e => (width = carousel.offsetWidth));
+
+$('.menu-mobile').click(function(){
+  $('.box-menu-mobile').toggleClass('show')
+})
